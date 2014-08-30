b50a430ae86d6fdbd8264c8eeab052a265dd3beb
deepubansal <deepu.bansal@gmail.com>
2014-08-30 21:37:36 +0530

    Created Program to print binary numbers upto length n.

---
+++ Classic/src/main/java/classic/algo/BinaryStrings.java
@@ -0,0 +1,24 @@
+package classic.algo;
+
+public class BinaryStrings {
+
+    // Program to print all the binary string of n bits.
+    public static void main(String[] args) {
+        int n = 4;
+        
+        printBinaryString("", n); 
+        
+    }
+
+    private static void printBinaryString(String prefix, int n) {
+        if (n == 1) {
+            System.out.println(prefix + "0");
+            System.out.println(prefix + "1");
+        }
+        else {
+            printBinaryString(prefix + "0", n-1);
+            printBinaryString(prefix + "1", n-1);
+        }
+        
+    }
+}
